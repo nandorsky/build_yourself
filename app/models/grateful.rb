a5f24f92cfaf29651b6f172a7f29aa1784aaeb98
Nate Andorsky <nandorsky@gmail.com>
2017-01-04 15:12:30 -0500

    front end updates

--- app/models/grateful.rb
+++ app/models/grateful.rb
@@ -1,3 +1,4 @@
 class Grateful < ActiveRecord::Base
 	belongs_to :day
+	validates :comment, :presence => true
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,4 @@
 class Target < ActiveRecord::Base
 	belongs_to :day
+	validates :target, :presence => true
 end
